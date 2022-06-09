a9d079f316ec2196ffa163ab0d986bca0ef8eef1
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 18:11:31 +0200

    Erste Funktionalität hinzugefügt

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,28 @@
-from arelle import CntlrCmdLine
+from arelle import Cntlr
 from arelle import PackageManager
+from arelle import ModelManager
+from arelle import ModelInstanceObject
+from arelle.ModelInstanceObject import ModelInlineFact
 
-class CntlrItegrated(CntlrCmdLine.CntlrCmdLine):
+import logging
+
+class CntlrItegrated(Cntlr.Cntlr):
     def __init__(self):
-        super().__init__()
+        super().__init__(logFileName="logToPrint")
 
 def main():
-    cntrl = CntlrItegrated()
+    cntlr = CntlrItegrated()
 
-    testTaxonomyPackageInfo = PackageManager.packageInfo(cntrl, "C:\\Users\\Moritz Klein\\OneDrive\\05 Studium\\Master\\4 S\\Masterarbeit\\04 Stichprobe\\volkswagenag\\META-INF\\taxonomyPackage.xml")
-    print(testTaxonomyPackageInfo["name"])
+    url_taxonomy = "C:/Users/Moritz Klein/OneDrive/05 Studium/Master/4 S/Masterarbeit/04 Stichprobe/volkswagenag/META-INF/taxonomyPackage.xml"
+    url_filing = "C:/Users/Moritz Klein/OneDrive/05 Studium/Master/4 S/Masterarbeit/04 Stichprobe/volkswagenag.xhtml"
+
+    modelManager = ModelManager.initialize(cntlr)
+
+    modelManager.load(url_filing, taxonomyPackages=[url_taxonomy])
+    
+    for fact in modelManager.modelXbrl.facts:
+        print(fact.qname)
+
 
 if __name__ == "__main__":
     main()
